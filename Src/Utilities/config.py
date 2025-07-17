9f3515b60f5549b5ba8e5c4f0a2ebb0cb8d190e3
nzo66 <nzo66.business@gmail.com>
2025-07-17 06:37:43 +0200

    Update config.py

--- Src/Utilities/config.py
+++ Src/Utilities/config.py
@@ -1,69 +1,86 @@
 #LOAD THE CONFIG
-import json
+import os
+from typing import Optional
 
-# Open the configuration file
-with open('config.json') as f:
-    # Load JSON data from file
-    config = json.load(f)
+def get_env_bool(key: str, default: str = "0") -> str:
+    """Get environment variable as string, defaulting to "0" for disabled"""
+    return os.getenv(key, default)
 
-# Accessing SC_DOMAIN
-SITE = config["Siti"]
-FT_DOMAIN = SITE["Filmpertutti"]['url']
-SC_DOMAIN = SITE["StreamingCommunity"]['url']
-TF_DOMAIN = SITE["Tantifilm"]['url']
-LC_DOMAIN = SITE["LordChannel"]['url']
-SW_DOMAIN = SITE["StreamingWatch"]['url']
-AW_DOMAIN = SITE['AnimeWorld']['url']
-SKY_DOMAIN = SITE['SkyStreaming']['url']
-CB_DOMAIN = SITE['CB01']['url']
-DDL_DOMAIN = SITE['DDLStream']['url']
-DLHD_DOMAIN = SITE['DaddyLiveHD']['url']
-GS_DOMAIN = SITE['Guardaserie']['url']
-GHD_DOMAIN = SITE['GuardaHD']['url']
-OST_DOMAIN = SITE['Onlineserietv']['domain']
-SC = SITE['StreamingCommunity']['enabled']
-FT = SITE['Filmpertutti']['enabled']
-TF = SITE['Tantifilm']['enabled']
-LC = SITE['LordChannel']['enabled']
-SW = SITE['StreamingWatch']['enabled']
-AW = SITE['AnimeWorld']['enabled']
-SKY = SITE['SkyStreaming']['enabled']
-CB = SITE['CB01']['enabled']
-DDL = SITE['DDLStream']['enabled']
-MYSTERIUS = SITE['Mysterius']['enabled']
-GS = SITE['Guardaserie']['enabled']
-GHD = SITE['GuardaHD']['enabled']
-OST = SITE['Onlineserietv']['enabled']
-DLHD = SITE['DaddyLiveHD']['enabled']
-TF_ForwardProxy = SITE['Tantifilm']["TF_ForwardProxy"]
-SC_ForwardProxy = SITE['StreamingCommunity']["SC_ForwardProxy"]
-GS_ForwardProxy = SITE['Guardaserie']["GS_ForwardProxy"]
-GH_ForwardProxy = SITE['GuardaHD']["GH_ForwardProxy"]
-VX_ForwardProxy = SITE['StreamingCommunity']["VX_ForwardProxy"]
-AW_ForwardProxy = SITE['AnimeWorld']["AW_ForwardProxy"]
-MX_ForwardProxy = SITE['CB01']["MX_ForwardProxy"]
-CB_ForwardProxy = SITE['CB01']["CB_ForwardProxy"]
-OST_ForwardProxy = SITE['Onlineserietv']["OST_ForwardProxy"]
-GS_PROXY = SITE['Guardaserie']["GS_PROXY"]
-GH_PROXY = SITE['GuardaHD']["GH_PROXY"]
-TF_PROXY = SITE['Tantifilm']["TF_PROXY"]
-CB_PROXY = SITE['CB01']["CB_PROXY"]
-SC_PROXY = SITE['StreamingCommunity']["SC_PROXY"]
-VX_PROXY = SITE['StreamingCommunity']["VX_PROXY"]
-AW_PROXY = SITE['AnimeWorld']["AW_PROXY"]
-MX_PROXY = SITE['CB01']["MX_PROXY"]
-OST_PROXY = SITE['Onlineserietv']["OST_PROXY"]
-ips4_device_key = SITE['DDLStream']['cookies']["ips4_device_key"]
-ips4_IPSSessionFront = SITE['DDLStream']['cookies']["ips4_IPSSessionFront"]
-ips4_member_id = SITE['DDLStream']['cookies']["ips4_member_id"]
-ips4_login_key = SITE['DDLStream']['cookies']["ips4_login_key"]
-#General
-GENERAL = config['General']
-dotenv = GENERAL["load_env"]
-HOST = GENERAL["HOST"]
-PORT = GENERAL["PORT"]
-Icon = GENERAL["Icon"]
-Name = GENERAL["Name"]
-Public_Instance = GENERAL["Public_Instance"]
-Remote_Instance = GENERAL["Remote_Instance"]
-Global_Proxy =  GENERAL["Global_Proxy"]
+def get_env_str(key: str, default: str = "") -> str:
+    """Get environment variable as string"""
+    return os.getenv(key, default)
+
+def get_env_int(key: str, default: int = 0) -> int:
+    """Get environment variable as integer"""
+    try:
+        return int(os.getenv(key, str(default)))
+    except ValueError:
+        return default
+
+# Site URLs
+FT_DOMAIN = get_env_str("FT_URL", "https://filmpertutti.motorcycles")
+SC_DOMAIN = get_env_str("SC_URL", "https://streamingcommunity.paris")
+TF_DOMAIN = get_env_str("TF_URL", "https://www.tanti.bond")
+LC_DOMAIN = get_env_str("LC_URL", "https://lordchannel.net")
+SW_DOMAIN = get_env_str("SW_URL", "https://www.streamingwatch.org")
+AW_DOMAIN = get_env_str("AW_URL", "https://www.animeworld.so")
+SKY_DOMAIN = get_env_str("SKY_URL", "https://skystreaming.watch")
+CB_DOMAIN = get_env_str("CB_URL", "https://cb01new.loan")
+DDL_DOMAIN = get_env_str("DDL_URL", "https://ddlstreamitaly.co")
+DLHD_DOMAIN = get_env_str("DLHD_URL", "https://daddylive.mp")
+GS_DOMAIN = get_env_str("GS_URL", "https://guardaserietv.mom")
+GHD_DOMAIN = get_env_str("GHD_URL", "https://mostraguarda.stream")
+OST_DOMAIN = get_env_str("OST_DOMAIN", "com")
+
+# Site enabled flags
+SC = get_env_bool("SC_ENABLED", "1")
+FT = get_env_bool("FT_ENABLED", "1")
+TF = get_env_bool("TF_ENABLED", "1")
+LC = get_env_bool("LC_ENABLED", "1")
+SW = get_env_bool("SW_ENABLED", "1")
+AW = get_env_bool("AW_ENABLED", "1")
+SKY = get_env_bool("SKY_ENABLED", "1")
+CB = get_env_bool("CB_ENABLED", "1")
+DDL = get_env_bool("DDL_ENABLED", "1")
+MYSTERIUS = get_env_bool("MYSTERIUS_ENABLED", "0")
+GS = get_env_bool("GS_ENABLED", "1")
+GHD = get_env_bool("GHD_ENABLED", "1")
+OST = get_env_bool("OST_ENABLED", "1")
+DLHD = get_env_bool("DLHD_ENABLED", "1")
+
+# Proxy configurations
+TF_ForwardProxy = get_env_bool("TF_FORWARD_PROXY", "0")
+SC_ForwardProxy = get_env_bool("SC_FORWARD_PROXY", "0")
+GS_ForwardProxy = get_env_bool("GS_FORWARD_PROXY", "0")
+GH_ForwardProxy = get_env_bool("GHD_FORWARD_PROXY", "0")
+VX_ForwardProxy = get_env_bool("VX_FORWARD_PROXY", "0")
+AW_ForwardProxy = get_env_bool("AW_FORWARD_PROXY", "0")
+MX_ForwardProxy = get_env_bool("MX_FORWARD_PROXY", "0")
+CB_ForwardProxy = get_env_bool("CB_FORWARD_PROXY", "0")
+OST_ForwardProxy = get_env_bool("OST_FORWARD_PROXY", "0")
+
+GS_PROXY = get_env_bool("GS_PROXY", "0")
+GH_PROXY = get_env_bool("GHD_PROXY", "0")
+TF_PROXY = get_env_bool("TF_PROXY", "0")
+CB_PROXY = get_env_bool("CB_PROXY", "0")
+SC_PROXY = get_env_bool("SC_PROXY", "0")
+VX_PROXY = get_env_bool("VX_PROXY", "0")
+AW_PROXY = get_env_bool("AW_PROXY", "0")
+MX_PROXY = get_env_bool("MX_PROXY", "0")
+OST_PROXY = get_env_bool("OST_PROXY", "0")
+
+# DDLStream cookies
+ips4_device_key = get_env_str("DDL_IPS4_DEVICE_KEY")
+ips4_IPSSessionFront = get_env_str("DDL_IPS4_IPSSESSIONFRONT")
+ips4_member_id = get_env_str("DDL_IPS4_MEMBER_ID")
+ips4_login_key = get_env_str("DDL_IPS4_LOGIN_KEY")
+
+# General configurations
+dotenv = get_env_bool("LOAD_ENV", "0")
+HOST = get_env_str("HOST", "0.0.0.0")
+PORT = get_env_int("PORT", 8080)
+Icon = get_env_str("ICON", "Pizza")
+Name = get_env_str("NAME", "MammaMia")
+Public_Instance = get_env_bool("PUBLIC_INSTANCE", "0")
+Remote_Instance = get_env_bool("REMOTE_INSTANCE", "1")
+Global_Proxy = get_env_bool("GLOBAL_PROXY", "0")
